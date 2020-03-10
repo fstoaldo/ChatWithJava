067789c15cdca7f2644764d19a860bc46bf84314
fstoaldo <fstoaldo@gmail.com>
2020-03-10 07:26:06 -0300

    started adding user functionalities

--- ChatServer/src/net/work/ServerThread.java
+++ ChatServer/src/net/work/ServerThread.java
@@ -2,10 +2,12 @@ package net.work;
 
 import java.io.*;
 import java.net.Socket;
+import org.apache.commons.lang3.StringUtils;
 
 public class ServerThread extends Thread {
 
     private final Socket clientSocket;
+    private String login = null;
 
     public ServerThread(Socket clientSocket) {
         this.clientSocket = clientSocket;
@@ -27,17 +29,52 @@ public class ServerThread extends Thread {
         InputStream inputStream = clientSocket.getInputStream();
         OutputStream outputStream = clientSocket.getOutputStream();
 
+        outputStream.write("Connection established\n".getBytes());
+
         // to read line by line
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String inputLine;
         while ((inputLine = reader.readLine()) != null) {
-            if ("quit".equalsIgnoreCase(inputLine)) {
-                break;
+            // split line into tokens
+            String[] tokens = StringUtils.split(inputLine);
+            // check tokens for null pointers
+            if (tokens != null && tokens.length > 0) {
+                // command will be the first token
+                String cmd = tokens[0];
+                if ("quit".equalsIgnoreCase(cmd)) {
+                    break;
+                }
+                // handle user logins
+                else if ("login".equalsIgnoreCase(cmd)) {
+                    handleLogin(outputStream, tokens);
+                }
+                else {
+                    // send message to outputStream
+                    String msg = "!UNKNOWN: <" + cmd + ">\n";
+                    outputStream.write(msg.getBytes());
+                }
             }
-            // send message to outputStream
-            String msg = "ECHO: " + inputLine + "\n";
-            outputStream.write(msg.getBytes());
         }
         clientSocket.close();
+    }
+
+    private void handleLogin(OutputStream outputStream, String[] tokens) throws IOException {
+        if (tokens.length == 3) {
+            // login <username> <passwd>
+            String login = tokens[1];
+            String passwd = tokens[2];
+
+            // allow for guest logins
+            if (login.equals("guest") && passwd.equals("guest")) {
+                String msg = "Login OK\n";
+                outputStream.write(msg.getBytes());
+                // tie this thread to the login
+                this.login = login;
+                System.out.println("User logged in: " + login);
+            } else {
+                String msg = "login ERROR\n";
+                outputStream.write(msg.getBytes());
+            }
+        }
     }
 }
